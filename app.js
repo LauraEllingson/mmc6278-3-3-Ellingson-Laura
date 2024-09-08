c8a958bd8193bf359c83025903afc0703b058cb4
LauraEllingson <lellingson@ufl.edu>
2024-09-08 15:52:41 -0400

    solved

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-// TODO: import the getCityInfo and getJobs functions from util.js
-import { getCityInfo, getJobs } from './util.js'; 
+const express = require('express'); // Importing the Express library to create the server
+const path = require('path'); // Importing the Path library to handle file paths
+const { getCityInfo, getJobs } = require('./util'); // Importing helper functions from util.js
+
+const app = express(); // Creating an Express application
+
+//  serve static files  from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to handle API requests for city and job information
+app.get('/api/city/:city', async (req, res) => {
+  const city = req.params.city; // Extracts the city name from the URL
+
+  try {
+    // Call helper functions to get city information and job listings based on the city
+    const cityInfo = await getCityInfo(city);
+    const jobs = await getJobs(city);
+
+    // If no data is found for both cityInfo and jobs, send a 404 status
+    if (!cityInfo && !jobs) {
+      return res.status(404).json({ error: 'No Jobs or City Found' });
+    }
+
+    // If data is found, send it as a JSON response with a 200 status
+    res.status(200).json({ cityInfo, jobs });
+  } catch (error) {
+    //  handle  error codes
+    if (error.response && [401, 403, 404, 500].includes(error.response.status)) {
+      return res.status(404).json({ error: `Not Found: ${error.response.status}` });
+    }
+
+    // generic error
+    res.status(500).json({ error: 'Generic Server Error' });
+  }
+});
+
+module.exports = app;
 
 
-
-// TODO: Statically serve the public folder
-
-// TODO: declare the GET route /api/city/:city
-// This endpoint should call getCityInfo and getJobs and return
-// the result as JSON.
-// The returned JSON object should have two keys:
-// cityInfo (with value of the getCityInfo function)
-// jobs (with value of the getJobs function)
-// If no city info or jobs are found,
-// the endpoint should return a 404 status
-
-module.exports = app
